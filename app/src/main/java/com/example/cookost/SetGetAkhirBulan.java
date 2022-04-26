5e2afe61ccd25000ac03cf95903dbee4efd20af8
f4moUSe <mhanifp27@gmail.com>
2022-04-26 23:09:50 +0700

    recycleView Data Akhir Bulan
    Nih Ngabs

---
+++ app/src/main/java/com/example/cookost/SetGetAkhirBulan.java
@@ -0,0 +1,22 @@
+package com.example.cookost;
+
+public class SetGetAkhirBulan {
+    private String NamaAkhirBulan;
+    private int FotoAkhirBulan;
+
+    public String getNamaAkhirBulan() {
+        return NamaAkhirBulan;
+    }
+
+    public void setNamaAkhirBulan(String namaAkhirBulan) {
+        NamaAkhirBulan = namaAkhirBulan;
+    }
+
+    public int getFotoAkhirBulan() {
+        return FotoAkhirBulan;
+    }
+
+    public void setFotoAkhirBulan(int fotoAkhirBulan) {
+        FotoAkhirBulan = fotoAkhirBulan;
+    }
+}
